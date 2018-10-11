4867f472f74babe566ea55520fadc72bb5dd65c4
James Ullom <jamesullom@Jamess-MacBook-Pro-2.local>
2018-10-11 13:26:16 -0600

    Initial Commit after converting from Nav to Tabbed View Controller navigation

---
+++ Shortcut-Tabbed/Controllers/OceanVC.swift
@@ -0,0 +1,30 @@
+//
+//  OceanVC.swift
+//  Shortcut
+//
+//  Created by James Ullom on 10/11/18.
+//  Copyright © 2018 Hammer of the Gods Software. All rights reserved.
+//
+
+import UIKit
+
+class OceanVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
